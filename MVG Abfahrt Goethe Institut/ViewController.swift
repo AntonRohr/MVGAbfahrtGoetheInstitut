c71081f627e12f250ffa53b23d12eccdb94830ab
Anton Rohr <anton.rohr@gmx.de>
2018-08-17 14:00:32 +0200

    Initial Commit

---
+++ MVG Abfahrt Goethe Institut/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVG Abfahrt Goethe Institut
+//
+//  Created by Anton Rohr on 17.08.18.
+//  Copyright © 2018 Anton Rohr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
